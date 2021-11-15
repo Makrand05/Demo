4a9503126973c7288553576d30ac2dca440219bc
Makrand <makrandshingare@gmail.com>
2021-11-15 09:16:32 +0530

    [Makrand] Added while and if condition

--- trail.sh
+++ trail.sh
@@ -1,3 +1,13 @@
 #! /bin/bash -x
-
-echo "Git Testing "
+number=10
+while [ $number -gt 0 ]
+do
+	a=$((RANDOM%2))
+	if [ $a -eq 1 ]
+	then
+		echo " congratulation you got the result "
+	else
+		echo " you not the result"
+	fi
+	((number--))
+done
